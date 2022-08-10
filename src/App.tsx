0a8fec9590cae8f946b1bf130a46e5c591ddcfaf
Vincent Ahrend <mail@vincentahrend.com>
2022-08-10 22:07:09 +0200

    Add catch-all redirect
    A temporary fix for https://github.com/interalia-studio/fullscreen/issues/70

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import { Home } from "~/pages/Home";
 import { Board } from "~/pages/Board";
@@ -13,7 +18,8 @@ const App = () => {
       <Routes>
         <Route path="/board" element={<Board />} />
         <Route path="/board/:boardId" element={<Board />} />
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
